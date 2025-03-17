5e3d47cedcfdf78cde049035f8219d178d31ba8d
lanehowell <howell.lane123@gmail.com>
2025-03-17 07:10:16 -0500

    Update config

--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser'
 import { SCENE_KEYS } from './scene-keys'
-import { BATTLE_BACKGROUND_ASSET_KEYS, MONSTER_ASSET_KEYS } from '../assets/asset-keys'
+import { BATTLE_ASSET_KEYS, BATTLE_BACKGROUND_ASSET_KEYS, HEALTH_BAR_ASSET_KEYS, MONSTER_ASSET_KEYS } from '../assets/asset-keys'
 
 export class BattleScene extends Phaser.Scene {
     constructor() {
@@ -19,5 +19,24 @@ export class BattleScene extends Phaser.Scene {
         // RENDER OUT PLAYER AND ENEMY MONSTERS
         this.add.image(768, 144, MONSTER_ASSET_KEYS.CARNODUSK, 0)
         this.add.image(256, 316, MONSTER_ASSET_KEYS.IGUANIGNITE, 0).setFlipX(true)
+
+        // RENDER OUT THE PLAYER HEALTH BAR
+        const playerMonsterName = this.add.text(20, 20, MONSTER_ASSET_KEYS.IGUANIGNITE, {
+            color: '#7E3D3F',
+            fontSize: '32px'
+        })
+
+        this.add.container(556, 318, [
+            this.add.image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND).setOrigin(0),
+            playerMonsterName,
+            this.createHealth(34,34)
+        ])
+    }
+
+    createHealth(x, y){
+        const leftCap = this.add.image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
+        const middle = this.add.image(leftCap.xx, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
+        const rightCap = this.add.image(x, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP)
+        return this.add.container(x,y, [leftCap, middle, rightCap])
     }
 }
--- vite.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    base: '/Monster-Catchers/', // Replace with your actual repository name
-});
